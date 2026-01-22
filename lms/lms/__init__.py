1fdb4f38361d29e57933eaf54e5ec3ed2d10e00d
shahnawaz-soft <shahnawaz@vecube.club>
2026-01-22 12:24:41 +0530

    Updated license creation

--- lms/lms/__init__.py
+++ lms/lms/__init__.py
@@ -2,7 +2,7 @@ import django
 from django.conf import settings
 
 # ✅ Setup Django before Celery is loaded
-django.setup()
+# django.setup()
 
 from .celery import app as celery_app  # Import Celery AFTER Django setup
 
