dbf187f60fc295cd8348089c04325f44886b2ff9
shahnawaz-soft <shahnawaz@vecube.club>
2026-01-22 12:12:44 +0530

    Updated license creation

--- lms/lms/settings/base.py
+++ lms/lms/settings/base.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import Config, RepositoryEnv
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
